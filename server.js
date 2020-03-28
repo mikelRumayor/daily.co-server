eaf369dcb45b6fb58b9a09668f4f96c214ffd799
Mikel Rumayor <mikel@joinedapp.com>
2020-03-28 14:35:25 -0700

    adding cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const rooms = require('./routes/rooms')
+
+app.use(cors())
 
 const router = new express.Router();
 
