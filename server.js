aa49a6b0f90b952466ff01f52e922ab8c7afc6bd
Mikel Rumayor <mikel@joinedapp.com>
2020-03-28 18:25:14 -0700

    adding stats endponts

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const database = require('./providers/database')
+
+
 const rooms = require('./routes/rooms')
+const stats = require('./routes/stats')
 
 app.use(cors())
+app.use(bodyParser.json())
 
 const router = new express.Router();
 
 router.use(rooms)
+router.use(stats)
 
 app
   .get('/', (req, res) => res.send('OK'))
